dcf69d7404a9a3e52daf732b8e61c53436266a55
airmanxtw <airmanx@stust.edu.tw>
2024-08-01 14:39:24 +0800

    feat: Update Preact integration for dynamic rendering

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ import { h } from "https://esm.sh/preact";
 import { useState } from "https://esm.sh/preact/hooks";
 import htm from "https://esm.sh/htm";
 export const useApp = () => {
-    
   const html = htm.bind(h);
 
-  const App = (props) => {
+  const App = ({ show, onClick }) => {
     const [name, setName] = useState(new Date().toLocaleTimeString());
     setTimeout(() => {
       setName(new Date().toLocaleTimeString());
     }, 1000);
-    return html`${props.show ? html`<h1>${name}</h1>` : html`<h2>Hello ${name}</h2>`} `;
+    return html`${show ? html`<h1>${name}</h1>` : html`<h2>Hello ${name}</h2>`} <br />
+      <button onClick=${() => onClick(new Date().toLocaleTimeString())}>GET TIME</button> `;
   };
 
   return { App };
